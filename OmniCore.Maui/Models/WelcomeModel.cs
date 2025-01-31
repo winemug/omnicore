9ad77aefdb1071dc6be70197de8d0166868b7200
Baris Kurtlutepe <barisk@gmail.com>
2025-01-31 23:24:56 +0100

    sample ui events and cmds

--- OmniCore.Maui/Models/WelcomeModel.cs
+++ OmniCore.Maui/Models/WelcomeModel.cs
@@ -1,9 +1,35 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using OmniCore.Services.Interfaces.Platform;
 
 namespace OmniCore.Maui.Models;
 
-public class WelcomeModel(IPlatformService platformService, IPlatformInfo platformInfo)
+public partial class WelcomeModel(IPlatformService platformService, IPlatformInfo platformInfo)
     : ObservableObject
 {
+
+    private bool _isNavigatedTo;
+
+    [RelayCommand]
+    private void NavigatedTo() =>
+        _isNavigatedTo = true;
+
+    [RelayCommand]
+    private void NavigatedFrom() =>
+        _isNavigatedTo = false;
+
+    [RelayCommand]
+    private async Task Appearing()
+    {
+        if (_isNavigatedTo)
+        {
+            // refresh etc
+        }
+    }
+    
+    [RelayCommand]
+    private async Task Kommando()
+    {
+    }
+
 }
