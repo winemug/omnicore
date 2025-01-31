9b1c60f3358aeb87217680a819f5d1aa59e2e727
Baris Kurtlutepe <barisk@gmail.com>
2025-01-31 23:00:48 +0100

    delete unused platforms

--- OmniCore.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace OmniCore.Maui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
