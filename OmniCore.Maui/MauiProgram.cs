194240725f189b5036846537d4765815a3ec1ec8
Baris Kurtlutepe <barisk@gmail.com>
2025-01-31 22:41:48 +0100

    more template rel. changes / refactoring

--- OmniCore.Maui/MauiProgram.cs
+++ OmniCore.Maui/MauiProgram.cs
@@ -1,40 +1,33 @@
-﻿//using CommunityToolkit.Maui;
+﻿using CommunityToolkit.Maui;
+using Fonts;
 using Microsoft.Extensions.Logging;
-using OmniCore.Common.Api;
-using OmniCore.Common.Data;
-using OmniCore.Maui.Services;
-using OmniCore.Maui.ViewModels;
-using OmniCore.Maui.Views;
-using OmniCore.Services;
-using OmniCore.Services.Interfaces.Core;
 using OmniCore.Services.Interfaces.Platform;
-using OmniCore.Services.Interfaces.Pod;
-using OmniCore.Services.Interfaces.Radio;
 
 namespace OmniCore.Maui;
 
 public static class MauiProgram
 {
-    public static MauiApp CreateMauiApp()
-    {
-        return CreateMauiApp(null, null);
-    }
-
     public static MauiApp CreateMauiApp(IPlatformInfo platformInfo, IPlatformService platformService)
     {
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            //.UseMauiCommunityToolkit()
+            .UseMauiCommunityToolkit()
             .RegisterAppServices(platformInfo, platformService)
+            .ConfigureMauiHandlers(handlers =>
+            {
+            })
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("SegoeUI-Semibold.ttf", "SegoeSemibold");
+                fonts.AddFont("FluentSystemIcons-Regular.ttf", FluentUI.FontFamily);
             });
 
 #if DEBUG
         builder.Logging.AddDebug();
+        builder.Services.AddLogging(configure => configure.AddDebug());
 #endif
 
         return builder.Build();
@@ -43,31 +36,8 @@ public static class MauiProgram
     public static MauiAppBuilder RegisterAppServices(this MauiAppBuilder mauiAppBuilder,
         IPlatformInfo platformInfo, IPlatformService platformService)
     {
-	    
-        mauiAppBuilder.AddMapping<WelcomePage, WelcomeViewModel>();
-        mauiAppBuilder.AddMapping<TestPage, TestViewModel>();
-	    
-	    mauiAppBuilder.Services.AddSingleton<AppShell>();
-        mauiAppBuilder.Services.AddSingleton<IAppConfiguration, AppConfiguration>();
-        
-        mauiAppBuilder.Services.AddSingleton<IPodService, PodService>();
-        mauiAppBuilder.Services.AddSingleton<IRadioService, RadioService>();
-        mauiAppBuilder.Services.AddSingleton<IAmqpService, AmqpService>();
-        mauiAppBuilder.Services.AddSingleton<ISyncService, SyncService>();
-        mauiAppBuilder.Services.AddSingleton<IRaddService, RaddService>();
-        mauiAppBuilder.Services.AddSingleton<ICoreService, CoreService>();
-        mauiAppBuilder.Services.AddSingleton<RaddService>();
-        mauiAppBuilder.Services.AddDbContext<OcdbContext>();
-        
-        mauiAppBuilder.Services.AddTransient<IRadio, Radio>();
-        mauiAppBuilder.Services.AddTransient<IRadioConnection, RadioConnection >();
-        mauiAppBuilder.Services.AddTransient<IPodModel, PodModel>();
-        mauiAppBuilder.Services.AddTransient<IPodConnection, PodConnection>();
-        mauiAppBuilder.Services.AddTransient<IPodPacket, PodPacket>();
-        mauiAppBuilder.Services.AddTransient<IApiClient, ApiClient>();
-
-        mauiAppBuilder.Services.AddSingleton(typeof(IPlatformService), platformService);
-        mauiAppBuilder.Services.AddSingleton(typeof(IPlatformInfo), platformInfo);
+        mauiAppBuilder.Services.AddSingleton<IPlatformService>(platformService);
+        mauiAppBuilder.Services.AddSingleton<IPlatformInfo>(platformInfo);
 
         return mauiAppBuilder;
     }
