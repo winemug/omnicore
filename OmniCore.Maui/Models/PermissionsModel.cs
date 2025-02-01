f9a0dd0d347531c28fa32cfdc62ef5436c22bf56
Baris Kurtlutepe <barisk@gmail.com>
2025-02-01 01:40:36 +0100

    simple permissions display

--- OmniCore.Maui/Models/PermissionsModel.cs
+++ OmniCore.Maui/Models/PermissionsModel.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -5,29 +6,54 @@ namespace OmniCore.Maui.Models;
 
 public partial class PermissionsModel : PageModel
 {
-    [ObservableProperty] PermissionStatus? _permission1Status;
-    [ObservableProperty] PermissionStatus? _permission2Status;
+    [ObservableProperty] List<DynamicPermission> _dynamicPermissions =
+    [
+        new DynamicPermission("android.permission.ACCESS_NETWORK_STATE", false),
+        new DynamicPermission("android.permission.INTERNET", false),
+        new DynamicPermission("android.permission.BLUETOOTH_CONNECT", false),
+        new DynamicPermission("android.permission.BLUETOOTH_SCAN", false),
+        new DynamicPermission("android.permission.POST_NOTIFICATIONS", false),
+        new DynamicPermission("android.permission.FOREGROUND_SERVICE", false),
+        new DynamicPermission("android.permission.FOREGROUND_SERVICE_SYSTEM_EXEMPTED", false),
+        new DynamicPermission("android.permission.SCHEDULE_EXACT_ALARM", false),
+    ];
 
     protected override async Task Appearing()
     {
         await Check();
+        base.OnPropertyChanged(nameof(DynamicPermissions));
     }
 
     private async Task Check()
     {
-        Permission1Status = await Permissions.CheckStatusAsync<Permissions.Bluetooth>();
-        Permission2Status = await Permissions.CheckStatusAsync<Permissions.PostNotifications>();
+        foreach (var permission in _dynamicPermissions)
+        {
+            await permission.UpdateStatusAsync();
+        }
     }
-    [RelayCommand]
-    private async Task RequestPermission1()
+}
+public class DynamicPermission(string permissionName, bool isRuntime) : Permissions.BasePlatformPermission
+{
+    private PermissionStatus _lastStatus = PermissionStatus.Unknown;
+    public string ShortName => permissionName.Substring(permissionName.LastIndexOf('.') + 1);
+    public bool IsGranted => _lastStatus == PermissionStatus.Granted;
+    public override string ToString()
     {
-        Permission1Status = await Permissions.RequestAsync<Permissions.Bluetooth>();
+        return $"{permissionName}, {isRuntime} = {_lastStatus}";
     }
-    
-    [RelayCommand]
-    private async Task RequestPermission2()
+    public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new[]
     {
-        Permission2Status = await Permissions.RequestAsync<Permissions.PostNotifications>();
+        (permissionName, isRuntime),
+    };
+
+    public async Task UpdateStatusAsync()
+    {
+        _lastStatus = await CheckStatusAsync();
+    }
+
+    public async Task RequestAndUpdateStatusAsync()
+    {
+        _lastStatus = await RequestAsync();
     }
 
 }
